cfeb48111316d28834ffc485680243e25391f42b
Stan Seibert <sseibert@anaconda.com>
2020-05-04 13:30:12 -0500

    Disable Python 2.7 testing and raise minimum to Python 3.4

--- setup.py
+++ setup.py
@@ -44,5 +44,5 @@ setup(name=NAME,
       zip_safe=False,
       install_requires=['pyyaml'],
       tests_require=['pytest'],
-      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+      python_requires='>=3.4,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
       )
